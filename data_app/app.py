28ed7f249e8850e3534ea3567e223ef93ec818f5
Tran Hoang Nguyen <tranhoangnguyen03@gmail.com>
2023-03-29 14:38:09 +0700

    .

--- data_app/app.py
+++ data_app/app.py
@@ -6,28 +6,38 @@ PAGE_CONFIG ={"page_title":app_name,"layout":"wide"}
 st.set_page_config(**PAGE_CONFIG) 
 
 def main():
+    st.headr('Check Environment Variables')
     st.write(f'{os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")=}')
     st.write(f'{os.environ.get("SECRET_KEY")=}')
     st.write(f'{os.environ.get("PROJECT_ID")=}')
     st.write(f'{os.environ.get("GOOGLE_CLOUD_PROJECT")=}')
     
     try:
-        with open("$HOME/.config/gcloud/application_default_credentials.json") as f:
+        with open("/.config/gcloud/application_default_credentials.json") as f:
                 st.write(f.read())
     except:
         pass
     
+    st.header('Check /.config/gcloud/application_default_credentials.json')
     from google.oauth2 import service_account
     from googleapiclient.discovery import build
 
     try:
         credentials = service_account.Credentials.from_service_account_file(
-        '$HOME/.config/gcloud/application_default_credentials.json'
+        '/.config/gcloud/application_default_credentials.json'
         )
 
         service = build('gmail', 'v1', credentials=credentials)
 
         st.write(f'{service=}')
+    except:
+        pass
+
+    st.header('Check Application Default Credential via google.auth')
+    try:
+        import google.auth
+        credentials, project_id = google.auth.default() 
+        st.write(f'{credentials=}')
     except:
         pass
 
